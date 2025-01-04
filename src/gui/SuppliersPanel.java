cea076eb5cd317100bc6d93cdd5e22ffcb463eb2
Isik Sezgin <isik.sezgin@std.yeditepe.edu.tr>
2025-01-04 03:07:42 +0300

    crud operations completed

--- src/gui/SuppliersPanel.java
+++ src/gui/SuppliersPanel.java
@@ -1,9 +1,13 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -13,6 +17,13 @@ import structures.Supplier;
 public class SuppliersPanel extends IMSPanel {
 	private static final long serialVersionUID = 1L;
 	private SupplierRepo repo;
+	private JTextField idField;
+	private JTextField nameField;
+	private JTextField contactNoField;
+	private JTextField addressField;
+	private JTextField resultField;
+	private JCheckBox isActiveField ;
+
 	
 	public SuppliersPanel(SupplierRepo repo) {
 		this.repo = repo;
@@ -39,27 +50,162 @@ public class SuppliersPanel extends IMSPanel {
 		return new String[]{Integer.toString(s.id), s.name, s.contactNumber, s.address, Boolean.toString(s.isActive)};
 	}
 	
-	public void initializeForm(JPanel boxPanel) {
-		JTextField idField = new JTextField();
-		JTextField nameField = new JTextField();
-		JTextField contactNumberField = new JTextField();
-		JTextField addressField = new JTextField();
-		JTextField isActiveField = new JTextField();
+	public void initializeForm(JPanel formPanel) {
+		formPanel.setLayout(null);
 		
-		boxPanel.add(new JLabel("Id"));
-		boxPanel.add(idField);
-		boxPanel.add(new JLabel("Name"));
-		boxPanel.add(nameField);
-		boxPanel.add(new JLabel("Contact Number"));
-		boxPanel.add(contactNumberField);
-		boxPanel.add(new JLabel("Address"));
-		boxPanel.add(addressField);
-		boxPanel.add(new JLabel("Is It Active"));
-		boxPanel.add(isActiveField);
-		boxPanel.add(new JButton("save"));
-	
+		JLabel lblId = new JLabel("Supplier Id");
+		lblId.setBounds(52, 40, 102, 15);
+		formPanel.add(lblId);
+		
+		idField = new JTextField();
+		idField.setBounds(184, 36, 185, 19);
+		formPanel.add(idField);
+		idField.setColumns(10);
+		
+		JLabel lblId_1 = new JLabel("Name");
+		lblId_1.setBounds(52, 69, 102, 15);
+		formPanel.add(lblId_1);
+		
+		nameField = new JTextField();
+		nameField.setColumns(10);
+		nameField.setBounds(184, 65, 185, 19);
+		formPanel.add(nameField);
+		
+		JLabel lblId_2 = new JLabel("Contact No");
+		lblId_2.setBounds(52, 98, 102, 15);
+		formPanel.add(lblId_2);
+		
+		contactNoField = new JTextField();
+		contactNoField.setColumns(10);
+		contactNoField.setBounds(184, 94, 185, 19);
+		formPanel.add(contactNoField);
+		
+		JLabel lblId_3 = new JLabel("Address");
+		lblId_3.setBounds(52, 127, 102, 15);
+		formPanel.add(lblId_3);
+		
+		addressField = new JTextField();
+		addressField.setColumns(10);
+		addressField.setBounds(184, 123, 185, 19);
+		formPanel.add(addressField);
 		
 		
+		isActiveField = new JCheckBox("Is Active");
+		isActiveField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		isActiveField.setBounds(184, 150, 129, 23);
+		formPanel.add(isActiveField);
+		
+		
+		JButton updateBtn = new JButton("Update");
+		updateBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Supplier supplier = readForm();
+				String message = repo.update(supplier);
+				JOptionPane.showMessageDialog(SuppliersPanel.this, message);
+				resultField.setText(message);
+			}
+		});
+		updateBtn.setBounds(260, 207, 97, 25);
+		formPanel.add(updateBtn);
+		
+		JButton readBtn = new JButton("Read");
+		readBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Supplier supplier = readForm();
+				Supplier supplier2 = repo.read(supplier.id);
+				if (supplier2 == null) {
+					String message = "Record not found: " + supplier.id;
+					resultField.setText(message);
+					JOptionPane.showMessageDialog(SuppliersPanel.this, message  );
+					return;
+				}
+				setForm(supplier2);
+				
+			}
+		});
+		readBtn.setBounds(151, 207, 97, 25);
+		formPanel.add(readBtn);
+		
+		JButton createBtn = new JButton("Create");
+		createBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+//				if(idField.getText()!= null && !idField.getText().isBlank()) {
+//					resultField.setText("id alanı boş olmalıdır");
+//					return;
+//				}
+				Supplier supplier = readForm();
+				String message = repo.create(supplier);
+				JOptionPane.showMessageDialog(SuppliersPanel.this, message);
+				resultField.setText(message);
+			}
+		});
+		createBtn.setBounds(37, 207, 97, 25);
+		formPanel.add(createBtn);
+		
+		JButton deleteBtn = new JButton("Delete");
+		deleteBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Supplier supplier = readForm();
+				String message = repo.delete(supplier.id);
+				JOptionPane.showMessageDialog(SuppliersPanel.this, message);
+				resultField.setText(message);
+				clearForm();
+			
+			}
+		});
+		deleteBtn.setBounds(369, 207, 97, 25);
+		formPanel.add(deleteBtn);
+		
+		JButton clearBtn = new JButton("Clear Form");
+		clearBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				clearForm();
+			}
+		});
+		clearBtn.setBounds(197, 244, 117, 25);
+		formPanel.add(clearBtn);
+		
+		JLabel lblNewLabel = new JLabel("Result");
+		lblNewLabel.setBounds(37, 309, 70, 15);
+		formPanel.add(lblNewLabel);
+		
+		resultField = new JTextField();
+		resultField.setEditable(false);
+		resultField.setEnabled(true);
+		resultField.setBounds(97, 307, 369, 19);
+		formPanel.add(resultField);
+		resultField.setColumns(10);
+
+	}
+
+	protected void clearForm() {
+		idField.setText(null);
+		nameField.setText(null);
+		contactNoField.setText(null);
+		addressField.setText(null);
+		isActiveField.setSelected(false);
+		resultField.setText(null);		
+	}
+
+	protected void setForm(Supplier supplier) {
+		idField.setText(String.valueOf(supplier.id));
+		nameField.setText(supplier.name);
+		contactNoField.setText(supplier.contactNumber);
+		addressField.setText(supplier.address);
+		isActiveField.setSelected(supplier.isActive);
+	}
+
+	protected Supplier readForm() {
+		int id = Integer.parseInt(idField.getText().trim());
+		String name = nameField.getText().trim();
+		String contactNo = contactNoField.getText().trim();
+		String address = addressField.getText().trim();
+		boolean isActive = isActiveField.isSelected();
+		Supplier supplier = new Supplier(id, name, contactNo, address, isActive);
+		return supplier;
 	}
 	
 }
--- src/repo/SupplierRepo.java
+++ src/repo/SupplierRepo.java
@@ -2,7 +2,9 @@ package repo;
 
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,13 +21,13 @@ public class SupplierRepo {
 
 	public List<Supplier> listAll() {
 		try {
-			return fetchAll();
+			return listAllX();
 		}catch(Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
 	
-	private List<Supplier> fetchAll() throws Exception{
+	private List<Supplier> listAllX() throws Exception{
 		Statement st = con.createStatement();
 		String query="select * from suppliers";
 		ResultSet rs = st.executeQuery(query);
@@ -44,6 +46,76 @@ public class SupplierRepo {
 	    rs.close();
 	    st.close();
 	    return suppliers;
+	}
+	
+	public Supplier read(int id) {
+		String query="select * from suppliers WHERE id = ?";
+		Supplier supplier = null;
+		try {
+			PreparedStatement st = con.prepareStatement(query);
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+		    if (rs.next()) {
+		    	String name =rs.getString("name");
+		    	String contactNumber =rs.getString("contact_number");
+		    	String address =rs.getString("address");
+		    	boolean isActive =rs.getBoolean("is_active");
+		    	supplier = new Supplier(id, name, contactNumber, address, isActive);
+		    } 
+		    rs.close();
+		    st.close();
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	    return supplier;
+	}
+	
+
+	public String create(Supplier supplier) {
+		String sql = "INSERT INTO suppliers (id, name, contact_number, address, is_active) VALUES (?,?,?,?,?)";
+		try {
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setInt(1, supplier.id);
+			stmt.setString(2, supplier.name);
+			stmt.setString(3, supplier.contactNumber);
+			stmt.setString(4, supplier.address);
+			stmt.setInt(5, supplier.isActive ? 1 : 0);
+			stmt.executeUpdate();
+			
+		} catch(SQLException e) {
+			return e.getMessage();
+		}
+		return "Supplier created!";
+	}
+	
+	public String update(Supplier supplier) {
+		String sql = "UPDATE suppliers SET name = ?, contact_number = ?, address = ?, is_active = ? WHERE id = ?";
+		try {
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, supplier.name);
+			stmt.setString(2, supplier.contactNumber);
+			stmt.setString(3, supplier.address);
+			stmt.setInt(4, supplier.isActive ? 1 : 0);
+			stmt.setInt(5, supplier.id);
+			stmt.executeUpdate();
+			
+		} catch(SQLException e) {
+			return e.getMessage();
+		}
+		return "Supplier updated!";
+	}
+	
+	public String delete(int id) {
+		String sql = "DELETE FROM suppliers WHERE id = ?";
+		try {
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setInt(1, id);
+			stmt.executeUpdate();
+			
+		} catch(SQLException e) {
+			return e.getMessage();
+		}
+		return "Supplier deleted!";
 	}
 	
 }
